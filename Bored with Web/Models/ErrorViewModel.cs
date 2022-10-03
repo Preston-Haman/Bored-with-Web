3be02fb672f17a682dc7fdd6308b63335af32606
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-10-02 20:22:37 -0700

    Add project files.

---
+++ Bored with Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bored_with_Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
