04eae8549ca4fcd6f7dd2c7d40ae32ce569b1b35
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-11-19 00:39:57 -0800

    Create Basic Player Class

---
+++ Bored with Web/Games/Player.cs
@@ -0,0 +1,26 @@
+﻿namespace Bored_with_Web.Games
+{
+	public class Player
+	{
+		public int PlayerNumber { get; set; } = 0; //zero is invalid
+
+		public string Username { get; }
+
+		public bool Ready { get; set; } = false;
+
+		public Player(string username)
+		{
+			Username = username;
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return obj is Player p && Username == p.Username;
+		}
+
+		public override int GetHashCode()
+		{
+			return Username.GetHashCode();
+		}
+	}
+}
