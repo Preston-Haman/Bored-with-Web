a562345b77be9be32353ae6ed64ffc151463f333
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-12-08 18:43:27 -0800

    Create SimpleGameOutcome Class

---
+++ Bored with Web/Games/SimpleGameOutcome.cs
@@ -0,0 +1,67 @@
+﻿namespace Bored_with_Web.Games
+{
+	/// <summary>
+	/// The ending state of the game.
+	/// </summary>
+	public enum GameEnding : byte
+	{
+		/// <summary>
+		/// The game never started.
+		/// </summary>
+		NONE,
+
+		/// <summary>
+		/// The game was incomplete. This is likely due to the competing players leaving, or forfeiting.
+		/// </summary>
+		INCOMPLETE,
+
+		/// <summary>
+		/// The game ended in a draw.
+		/// </summary>
+		STALEMATE,
+
+		/// <summary>
+		/// The game ended with one or more players being victorious.
+		/// </summary>
+		VICTORY
+	}
+
+	/// <summary>
+	/// A class that represents the outcome of a game.
+	/// </summary>
+	public class SimpleGameOutcome
+	{
+		/// <summary>
+		/// The way the game ended.
+		/// <br></br><br></br>
+		/// If the game was incomplete, or ended in a stalemate, all players are considered
+		/// to be losing players (see <see cref="LosingPlayers"/>).
+		/// </summary>
+		public GameEnding EndState { get; set; }
+
+		/// <summary>
+		/// The game this outcome corresponds to.
+		/// </summary>
+		public SimpleGame Game { get; set; } = null!;
+
+		/// <summary>
+		/// The players that won the game.
+		/// </summary>
+		public HashSet<Player> WinningPlayers { get; set; } = new();
+
+		/// <summary>
+		/// The players that lost the game.
+		/// </summary>
+		public HashSet<Player> LosingPlayers { get; set; } = new();
+
+		/// <summary>
+		/// The players that gave up during this game.
+		/// </summary>
+		public HashSet<Player> ForfeitingPlayers { get; set; } = new();
+
+		/// <summary>
+		/// The number of turns taken by each player.
+		/// </summary>
+		public Dictionary<Player, int> PlayerTurnCounts { get; set; } = new();
+	}
+}
